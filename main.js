8446e9bb86193063bf4d71407ad37a795dc65b5a
lchw2021 <95357075+lchw2021@users.noreply.github.com>
2023-02-01 09:58:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -30,6 +30,14 @@ function placeHolder(i) {
 
 //opening functions
 function load12A(){
+  currentDate = new Date()
   url = 'https://rt.data.gov.hk/v1.1/transport/citybus-nwfb/eta/CTB/001087/12A'
-  document.getElementById('test').innerHTML = Get(url)
+  dict = JSON.loads(Get(url))
+  dataTime = dict['generated_timestamp']
+  [eta1,eta2,eta3] = [dict['data'][0]['eta'],dict['data'][1]['eta'],dict['data'][2]['eta']]
+  [time1,time2,time3] = [Date.parse(eta1)-currentDate,Date.parse(eta2)-currentDate,Date.parse(eta3)-currentDate]
+  min1 = Math.floor(time1/60000)
+  sec1 = Math.floor(time1/1000) % 60
+  stamp1 = min1.toString()+' min and '+sec1+' s'
+  document.getElementById('test').innerHTML = stamp1
 }
